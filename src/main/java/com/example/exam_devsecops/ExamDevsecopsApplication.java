c869192ee0ffd6eeb749ceb918daa64cfa47ad5a
anassea <moncompte246@gmail.com>
2024-10-28 09:21:20 +0100

    initialisation

---
+++ src/main/java/com/example/exam_devsecops/ExamDevsecopsApplication.java
@@ -0,0 +1,13 @@
+package com.example.exam_devsecops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamDevsecopsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExamDevsecopsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/exam_devsecops/ExamDevsecopsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.exam_devsecops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamDevsecopsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
